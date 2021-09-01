fb29c27358148e9e93a50c9217fc768b5ea6a62b
Aditya Pawar <a.balakrishnan@warriorlife.net>
2021-08-31 18:05:25 -0700

    arduino code working woth one motor

---
+++ arduino/arduino-timer-test/arduino-timer-test.ino
@@ -0,0 +1,93 @@
+#include <ros.h>
+#include <std_msgs/Int32.h>
+#include <arduino-timer.h>
+#include <RotaryEncoder.h>
+
+#define CHANNEL_A 2
+#define CHANNEL_B 3
+//#define INDEX 4
+
+#define DIRECTION 5
+#define PULSE 6
+
+#define MIN_PULSE_INTERVAL 100
+#define DEADZONE 5
+Timer<4, micros> timer;
+
+int encoder_position = 0, goal_position = 0, calls = 0, pulse_delay = 500;
+bool pulse = false, reached_position = false, pulse_value = HIGH, current_direction = LOW;
+
+RotaryEncoder encoder(CHANNEL_A, CHANNEL_B, RotaryEncoder::LatchMode::TWO03);
+ros::NodeHandle nh;
+std_msgs::Int32 int_msg;
+
+ros::Publisher encoder_value("right_arm_pitch_position", &int_msg);
+
+void callback(const std_msgs::Int32& msg) {
+  goal_position = msg.data;
+  current_direction = goal_position < encoder_position;
+  reached_position = false; 
+}
+
+void checkPosition() {
+  encoder.tick(); 
+  encoder_position = encoder.getPosition();
+  
+  if (abs(goal_position - encoder_position) < DEADZONE) {
+    reached_position = true; 
+  }
+
+  return true;
+}
+
+void send_encoder_data() {
+  int_msg.data = encoder_position;
+  encoder_value.publish(&int_msg);
+  return true;
+}
+
+bool motor_call(){
+  if(calls++ >= pulse_delay/MIN_PULSE_INTERVAL) {
+//    nh.logwarn("dsadas");
+    pulse = true;
+    calls = 0;
+  }
+  return true;
+}
+
+ros::Subscriber<std_msgs::Int32> arm_pitch_position("set_right_arm_pitch_position", &callback);
+
+void setup() {
+  //setting the pins to output for the motor
+  nh.initNode();
+  nh.subscribe(arm_pitch_position);
+  nh.advertise(encoder_value);
+  
+  while (!nh.connected()) {
+    nh.spinOnce();
+    delay(1);
+  }
+  
+  pinMode(PULSE, OUTPUT);
+  pinMode(DIRECTION, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_A), checkPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_B), checkPosition, CHANGE);
+
+  timer.every(100, motor_call);
+  timer.every(200000, send_encoder_data);
+}
+
+void loop() {
+  timer.tick<void>();
+
+  digitalWrite(DIRECTION, current_direction);
+  if (!reached_position) {
+    if (pulse) {
+      digitalWrite(PULSE, pulse_value);
+      pulse = false;
+      pulse_value = !pulse_value;  
+    }
+  }
+  nh.spinOnce();
+}
---
+++ arduino/stepper_ros_arduino/stepper_ros_arduino.ino
@@ -0,0 +1,204 @@
+
+#include <ros.h>
+#include <std_msgs/Int32.h>
+#include <arduino-timer.h>
+#include <RotaryEncoder.h>
+
+#define CHANNEL_A_PITCH 2
+#define CHANNEL_B_PITCH 3
+#define CHANNEL_A_YAW 18
+#define CHANNEL_B_YAW 19
+
+#define DIRECTION_PITCH 5
+#define PULSE_PITCH 6
+#define DIRECTION_YAW 7
+#define PULSE_YAW 8
+
+#define MIN_PULSE_INTERVAL 100
+#define DEADZONE 5
+
+RotaryEncoder yaw_encoder(CHANNEL_A_YAW, CHANNEL_B_YAW, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder pitch_encoder(CHANNEL_A_PITCH, CHANNEL_B_PITCH, RotaryEncoder::LatchMode::TWO03);
+
+ros::NodeHandle nh;
+
+struct PitchMotor {
+  int pulse_pin, direction_pin;
+  int encoder_position = 0, goal_position = 0, calls = 0, pulse_delay = 500;
+  bool pulse = false, reached_position = false, pulse_value = HIGH, current_direction = LOW;
+  
+  PitchMotor(int pulse_pin, int direction_pin) {
+    this->pulse_pin = pulse_pin;
+    this->direction_pin = direction_pin; 
+    
+//    pinMode(pulse_pin, OUTPUT);
+//    pinMode(direction_pin, OUTPUT);
+  }
+
+  void setGoalPosition(int goal_position) {
+    this->goal_position = goal_position;
+    current_direction = goal_position < encoder_position;
+    reached_position = false; 
+  }
+  
+  void checkPosition() {
+    pitch_encoder.tick(); 
+    encoder_position = pitch_encoder.getPosition();
+
+    if (abs(goal_position - encoder_position) < DEADZONE) {
+      reached_position = true; 
+    }
+
+  }
+
+  void motor_tick() {
+    digitalWrite(direction_pin, current_direction);
+    if (!reached_position) {
+      if (pulse) {
+        digitalWrite(pulse_pin, pulse_value);
+        pulse = false;
+        pulse_value = !pulse_value;  
+      }
+    }
+  }
+
+  void motor_call() {
+    if(calls++ >= pulse_delay/MIN_PULSE_INTERVAL) {
+      pulse = true;
+      calls = 0;
+    }
+  }
+  
+};
+
+struct YawMotor {
+  int pulse_pin, direction_pin;
+  int encoder_position = 0, goal_position = 0, calls = 0, pulse_delay = 500;
+  bool pulse = false, reached_position = false, pulse_value = HIGH, current_direction = LOW;
+  
+  YawMotor(int pulse_pin, int direction_pin) {
+    this->pulse_pin = pulse_pin;
+    this->direction_pin = direction_pin;
+
+//    pinMode(pulse_pin, OUTPUT);
+//    pinMode(direction_pin, OUTPUT);
+  }
+
+  void setGoalPosition(int goal_position) {
+    this->goal_position = goal_position;
+    current_direction = goal_position < encoder_position;
+    reached_position = false; 
+  }
+  
+  void checkPosition() {
+    yaw_encoder.tick(); 
+    encoder_position = yaw_encoder.getPosition();
+
+    if (abs(goal_position - encoder_position) < DEADZONE) {
+      
+      reached_position = true; 
+    }
+  }
+
+  void motor_tick() {
+    digitalWrite(direction_pin, current_direction);
+    if (!reached_position) {
+      if (pulse) {
+        digitalWrite(pulse_pin, pulse_value);
+        pulse = false;
+        pulse_value = !pulse_value;  
+      }
+    }
+  }
+
+  void motor_call() {
+    if(calls++ >= pulse_delay/MIN_PULSE_INTERVAL) {
+      pulse = true;
+      calls = 0;
+    }
+  } 
+};
+
+YawMotor yaw_motor(PULSE_YAW, DIRECTION_YAW);
+PitchMotor pitch_motor(PULSE_PITCH, DIRECTION_PITCH);
+
+Timer<4, micros> timer;
+
+std_msgs::Int32 pitch_msg;
+std_msgs::Int32 yaw_msg;
+
+ros::Publisher pitch_encoder_value("right_arm_pitch_position", &pitch_msg);
+ros::Publisher yaw_encoder_value("right_arm_yaw_position", &yaw_msg);
+
+
+void pitchCB(const std_msgs::Int32& msg) {
+  pitch_motor.setGoalPosition(msg.data);
+}
+
+void yawCB(const std_msgs::Int32& msg) {
+  yaw_motor.setGoalPosition(msg.data);
+}
+
+void checkYawPosition() {
+  yaw_motor.checkPosition();
+}
+
+void checkPitchPosition() {
+  pitch_motor.checkPosition();
+}
+
+void send_encoder_data() {
+  pitch_msg.data = pitch_motor.encoder_position;
+  pitch_encoder_value.publish(&pitch_msg);
+
+  yaw_msg.data = yaw_motor.encoder_position;
+  yaw_encoder_value.publish(&yaw_msg);
+  return true;
+}
+
+bool motor_call(){
+  yaw_motor.motor_call();
+  pitch_motor.motor_call();
+  return true;
+}
+
+ros::Subscriber<std_msgs::Int32> arm_pitch_position("set_right_arm_pitch_position", &pitchCB);
+ros::Subscriber<std_msgs::Int32> arm_yaw_position("set_right_arm_yaw_position", &yawCB);
+
+
+void setup() {
+  pinMode(PULSE_YAW, OUTPUT);
+  pinMode(DIRECTION_YAW, OUTPUT);
+  pinMode(PULSE_PITCH, OUTPUT);
+  pinMode(DIRECTION_PITCH, OUTPUT);
+  //setting the pins to output for the motor
+  nh.initNode();
+  nh.subscribe(arm_pitch_position);
+  nh.subscribe(arm_yaw_position);
+
+  nh.advertise(pitch_encoder_value);
+  nh.advertise(yaw_encoder_value);
+
+  
+  while (!nh.connected()) {
+    nh.spinOnce();
+    delay(1);
+  }
+ 
+
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PITCH), checkPitchPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PITCH), checkPitchPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_A_YAW), checkYawPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CHANNEL_B_YAW), checkYawPosition, CHANGE);
+
+  timer.every(100, motor_call);
+  timer.every(200000, send_encoder_data);
+}
+
+void loop() {
+  timer.tick<void>();
+
+  yaw_motor.motor_tick();
+  pitch_motor.motor_tick();
+  nh.spinOnce();
+}
