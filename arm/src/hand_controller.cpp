dbc03ad14cfee420aa7dbe53e57b4e02dcedd36d
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2021-09-10 21:29:04 -0700

    added hand control to cmake

--- arm/src/hand_controller.cpp
+++ arm/src/hand_controller.cpp
@@ -1,9 +1,16 @@
 #include <hand_controller/HandController.hpp>
+#include "DynamixelMotor.hpp"
+#include "DynamixelHelper.hpp"
 #include <ros/ros.h>
+
+struct AX12_TABLE : AddressTableBase() {
+    TORQ
+}
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "hand_controller");
     ros::NodeHandle nh;
 
-    
+
+
 }
